350e969a04f8b3deb135e6a8bbd471fd81552a19
Chris <chaltonj@microsoft.com>
2020-09-16 22:53:54 -0700

    fixed api for running

--- api/GetToken/index.js
+++ api/GetToken/index.js
@@ -1,29 +1,58 @@
 var axios = require('axios');
+// require('dotenv').config()
+const qs = require('qs');
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    if (!req.query.code) {
-        context.res = {
-            body: process.env["redirect_uri"]
+    context.log('JavaScript HTTP trigger  processed a request.');
+    if (req.query.code) {
+        let data = {
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: process.env["redirect_uri"],
+            client_id: process.env["client_id"],
+            client_secret: process.env["client_secret"]
         };
-    } else {
-        response = await axios.post("https://api.login.yahoo.com/oauth2/get_token",
-        {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            data: {
-                grant_type: 'authorization_code',
-                code: req.query.code,
-                redirect_uri: process.env["redirect_uri"],
-                client_id: process.env["client_id"],
-                client_secret: process.env["client_secret"]
-            }
+        await axios.post("https://api.login.yahoo.com/oauth2/get_token", qs.stringify(data))
+        .then(response => { 
+            console.log("success");
+            context.res = {
+                status: 200,
+                body: response.data
+            };
+        })
+        .catch(error => {
+            console.log("failure");
+            context.res = {
+                status: error.response.status,
+                body: error.response.data
+            };
+            console.log(error.response.data)
         });
-        console.log(response);
+    } else if (req.query.refresh_token) {
+        let data = {
+            grant_type: 'refresh_token',
+            refresh_token: req.query.refresh_token,
+            redirect_uri: process.env["redirect_uri"],
+            client_id: process.env["client_id"],
+            client_secret: process.env["client_secret"]
+        };
+        await axios.post("https://api.login.yahoo.com/oauth2/get_token", qs.stringify(data))
+        .then(response => { 
+            context.res = {
+                status: 200,
+                body: response.data
+            };
+        })
+        .catch(error => {
+            context.res = {
+                status: error.response.status,
+                body: error.response.data
+            };
+        });
+    } else {
         context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: response
+            status: 400,
+            body: "Code or refresh token required"
         };
     }
 }
--- src/Components/GetToken.tsx
+++ src/Components/GetToken.tsx
@@ -1,5 +1,5 @@
 import {
-    Link,
+    Button,
     TextField
     }
     from '@material-ui/core';
@@ -11,8 +11,6 @@ interface IGetTokenState {
     token: string
 }
 const redirectUri = "https://lemon-dune-0cd4b231e.azurestaticapps.net";
-
-
 const loginTitle = "Login with Yahoo! for Read Access";
 const loginUrl = "https://api.login.yahoo.com/oauth2/request_auth_fe?response_type=code&state=&client_id=dj0yJmk9QUJ2Yk1kQVdSbDZKJmQ9WVdrOVVXRmhXbGxtTm1zbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTM5&scope=&redirect_uri=" + redirectUri;
 const clientId = "dj0yJmk9QUJ2Yk1kQVdSbDZKJmQ9WVdrOVVXRmhXbGxtTm1zbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTM5";
@@ -39,37 +37,25 @@ export default class GetLink extends React.Component<any, IGetTokenState> {
     private get_token = (code: string) => {
         console.log("getting token");
         console.log(code);
-        axios.post("https://api.login.yahoo.com/oauth2/get_token",
-            {
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data: {
-                    grant_type: 'authorization_code',
-                    code: code,
-                    redirect_uri: redirectUri,
-                    client_id: clientId,
-                    client_secret: clientSecret
-                }
-            })
-            .then(res => {
-                if (res.status === 200) {
-                    console.log(res);
-                    console.log(res.data);
-                    console.log(res.data["access_token"])
-                } else {
-                    console.log("Failed get token: " + res);
-                }
-            });
+        axios.get('/api/GetToken', {
+            params: {
+              code: code
+            }
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
 
     public render() {
         return (
             <div>
-                <Link
-                    href={loginUrl}>{loginTitle}</Link>
-                <Link
-                    href={getTokenUrl}>{getTokenUrl}</Link>
+                <Button variant="contained" color="primary" href={loginUrl}>
+                    {loginTitle}
+                </Button>
                 <TextField
                     id="filled-multiline-flexible"
                     label="Multiline"
