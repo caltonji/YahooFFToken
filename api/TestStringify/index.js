bdb2ce6c07751e3f8d41ac0c52e686b797eb838a
Chris <chaltonj@microsoft.com>
2020-09-17 21:46:42 -0700

    avoid creating data

--- api/TestStringify/index.js
+++ api/TestStringify/index.js
@@ -8,14 +8,14 @@ module.exports = async function (context, req) {
             status: 200,
             body: req.query.code
         };
-        let data = {
-            grant_type: 'authorization_code',
-            code: eq.query.code,
-            redirect_uri: process.env["redirect_uri"],
-            client_id: process.env["client_id"],
-            client_secret: process.env["client_secret"]
-        };
-        context.log(data);
+        // let data = {
+        //     grant_type: 'authorization_code',
+        //     code: eq.query.code,
+        //     redirect_uri: process.env["redirect_uri"],
+        //     client_id: process.env["client_id"],
+        //     client_secret: process.env["client_secret"]
+        // };
+        // context.log(data);
         // context.res = {
         //     status: 200,
         //     body: qs.stringify(data)
