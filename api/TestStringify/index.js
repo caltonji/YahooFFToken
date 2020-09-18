6e38f14f7367d7378ac40746f45237479fe99768
Chris <chaltonj@microsoft.com>
2020-09-17 21:41:33 -0700

    changing value

--- api/TestStringify/index.js
+++ api/TestStringify/index.js
@@ -4,16 +4,21 @@ const qs = require('qs');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger  processed a request.');
     if (req.query.code) {
+        context.res = {
+            status: 200,
+            body: req.query.code
+        };
         let data = {
             grant_type: 'authorization_code',
-            code: String(req.query.code),
+            code: eq.query.code,
             redirect_uri: process.env["redirect_uri"],
             client_id: process.env["client_id"],
             client_secret: process.env["client_secret"]
         };
-        context.res = {
-            status: 200,
-            body: qs.stringify(data)
-        };
+        context.log(data);
+        // context.res = {
+        //     status: 200,
+        //     body: qs.stringify(data)
+        // };
     }
 }
